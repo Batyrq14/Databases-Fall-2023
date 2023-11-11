74ff08ebcda4a500ab2eaf273348a8b453f444ca
Batyrq14 <utegenov1403@gmail.com>
2023-11-11 15:53:51 +0600

    update

---
+++ Lab5/lab5.sql
@@ -0,0 +1,99 @@
+create database lab5;
+
+
+-- Create the "customers" table
+CREATE TABLE customers (
+    customer_id INTEGER PRIMARY KEY,
+    cust_name VARCHAR(255),
+    city VARCHAR(255),
+    grade numeric(3,0),
+    salesman_id INTEGER
+);
+
+-- Insert data into the "customers" table
+INSERT INTO customers (customer_id, cust_name, city, grade, salesman_id)
+VALUES
+    (3002, 'Nick Rimando', 'New York', 100, 5001),
+    (3005, 'Graham Zusi', 'California', 200, 5002),
+    (3001, 'Brad Guzan', 'London', NULL, 5005),
+    (3004, 'Fabian Johns', 'Paris', 300, 5006),
+    (3007, 'Brad Davis', 'New York', 200, 5001),
+    (3009, 'Geoff Camero', 'Berlin', 100, 5003),
+    (3008, 'Julian Green', 'London', 300, 5002);
+
+-- Create the "orders" table
+CREATE TABLE orders (
+    ord_no INTEGER PRIMARY KEY,
+    purch_amt DECIMAL(8, 2),
+    ord_date DATE,
+    customer_id INTEGER,
+    salesman_id INTEGER
+);
+
+-- Insert data into the "orders" table
+INSERT INTO orders (ord_no, purch_amt, ord_date, customer_id, salesman_id)
+VALUES
+    (70001, 150.5, '2012-10-05', 3005, 5002),
+    (70009, 270.65, '2012-09-10', 3001, 5005),
+    (70002, 65.26, '2012-10-05', 3002, 5001),
+    (70004, 110.5, '2012-08-17', 3009, 5003),
+    (70007, 948.5, '2012-09-10', 3005, 5002),
+    (70005, 2400.6, '2012-07-27', 3007, 5001),
+    (5760, NULL, '2012-09-10', 3002, 5001);
+
+-- Create the "salesman" table
+CREATE TABLE salesman (
+    salesman_id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    city VARCHAR(255),
+    commission DECIMAL(5, 2)
+);
+
+-- Insert data into the "salesman" table
+INSERT INTO salesman (salesman_id, name, city, commission)
+VALUES
+    (5001, 'James Hoog', 'New York', 0.15),
+    (5002, 'Nail Knite', 'Paris', 0.13),
+    (5005, 'Pit Alex', 'London', 0.11),
+    (5006, 'Mc Lyon', 'Paris', 0.14),
+    (5003, 'Lauson Hen', NULL, 0.12),
+    (5007, 'Paul Adam', 'Rome', 0.13);
+
+--total purchase amount of all orders.
+select sum(purch_amt) as total from orders;
+
+--average purchase amount of all orders.
+select avg(purch_amt) as average from orders;
+
+--how many customer have listed their names.
+select count(*) as total_customer_name
+from customers
+where cust_name is not null;
+
+--min purchase
+select min(purch_amt) as minimum from orders;
+
+--customer name with the letter 'b'
+select * from customers where cust_name like '%b';
+
+--customers from 'New York'
+select *
+from orders
+where customer_id in(select customer_id from customers where city = 'New York');
+
+--customers who has order with purchase amount more than 10
+select *
+from customers
+where customer_id in(select customer_id from orders where purch_amt > 10);
+
+--total grade of all customers
+select sum(grade) as total_grade from customers;
+
+--customers who have listed their names
+select *
+from customers
+where cust_name is not null;
+
+--maximum grade of all the customers
+select max(grade) as grade_max from customers;
+
---
+++ Lab6/lab6.sql
@@ -0,0 +1,116 @@
+--create database lab6;
+
+create table locations
+(
+    location_id serial primary key,
+    street_address varchar(25),
+    postal_code varchar(12),
+    city varchar(30),
+    state_province varchar(12)
+);
+
+create table departments
+(
+    department_id serial primary key ,
+    department_name varchar(50) unique ,
+    budget integer,
+    location_id integer references locations
+);
+
+create table employees
+(
+    employee_id serial primary key ,
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(50),
+    phone_number varchar(20),
+    salary integer,
+    department_id integer references departments
+);
+
+/*
+ "I generated random data for this database
+to practice my SQL skills and demonstrate
+my understanding of database operations.
+The data is entirely fictional and used
+for educational purposes."
+ */
+-- Insert data into the 'locations' table
+INSERT INTO locations (street_address, postal_code, city, state_province)
+VALUES
+    ('123 Main St', '12345', 'New York', 'NY'),
+    ('456 Elm St', '67890', 'Los Angeles', 'CA'),
+    ('789 Oak St', '54321', 'Chicago', 'IL'),
+    ('101 Pine St', '98765', 'San Francisco', 'CA'),
+    ('202 Maple St', '24680', 'Miami', 'FL'),
+    ('303 Cherry St', '13579', 'Seattle', 'WA'),
+    ('404 Walnut St', '86420', 'Boston', 'MA'),
+    ('505 Cedar St', '97531', 'Houston', 'TX'),
+    ('606 Birch St', '78901', 'Denver', 'CO'),
+    ('707 Pineapple St', '12340', 'Miami', 'FL');
+
+-- Insert data into the 'departments' table
+INSERT INTO departments (department_id, department_name, budget, location_id)
+VALUES
+    (10,'HR', 100000, 1),
+    (80,'Sales', 150000, 2),
+    (70,'Marketing', 120000, 3),
+    (50,'IT', 200000, 4),
+    (100,'Finance', 130000, 5),
+    (30,'Research', 90000, 6),
+    (60,'Customer Service', 110000, 7),
+    (90,'Engineering', 220000, 8),
+    (40,'Product Development', 160000, 9),
+    (20,'Quality Control', 95000, 10);
+
+-- Insert data into the 'employees' table
+INSERT INTO employees (first_name, last_name, email, phone_number, salary, department_id)
+VALUES
+    ('John', 'Smith', 'john.smith@example.com', '(123) 456-7890', 60000, 20),
+    ('Mary', 'Johnson', 'mary.johnson@example.com', '(234) 567-8901', 70000,null),
+    ('David', 'Williams', 'david.williams@example.com', '(345) 678-9012', 80000, 80),
+    ('Susan', 'Brown', 'susan.brown@example.com', '(456) 789-0123', 90000, 70),
+    ('Michael', 'Davis', 'michael.davis@example.com', '(567) 890-1234', 100000, 90),
+    ('Jennifer', 'Wilson', 'jennifer.wilson@example.com', '(678) 901-2345', 75000, 50),
+    ('Robert', 'Jones', 'robert.jones@example.com', '(789) 012-3456', 85000,null),
+    ('Linda', 'Martinez', 'linda.martinez@example.com', '(890) 123-4567', 72000, 30),
+    ('William', 'Clark', 'william.clark@example.com', '(901) 234-5678', 95000, 60),
+    ('Karen', 'Harris', 'karen.harris@example.com', '(012) 345-6789', 78000, 10);
+
+
+--3
+select first_name, last_name, department_id
+from employees;
+--3.1
+SELECT e.first_name, e.last_name, e.department_id, d.department_name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id;
+
+--4
+SELECT e.first_name, e.last_name, e.department_id, d.department_name
+FROM employees e
+JOIN departments d ON e.department_id = d.department_id
+WHERE d.department_id IN (80, 40);
+
+--5
+select e.first_name, e.last_name, d.department_name , l.city , l.state_province
+from employees e
+join departments d on e.department_id = d.department_id
+join locations l on d.location_id = l.location_id
+;
+
+--6
+select d.department_id,department_name,d.budget,d.location_id
+from departments d
+left join employees e on d.department_id = e.department_id;
+
+
+--7
+select e.first_name,e.last_name,d.department_id,d.department_name
+from employees e
+left join departments d on e.department_id = d.department_id;
+
+
+-- drop table employees cascade
+-- drop table locations cascade
+-- drop table departments cascade
